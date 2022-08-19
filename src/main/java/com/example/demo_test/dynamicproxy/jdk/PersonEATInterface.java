8949e6663a206fcdfebe7ff15336f1a218937e4c
qiwenbo <whutqwb@163.com>
2022-08-19 15:00:08 +0800

    动态代理

---
+++ src/main/java/com/example/demo_test/dynamicproxy/jdk/PersonEATInterface.java
@@ -0,0 +1,10 @@
+package com.example.demo_test.dynamicproxy.jdk;
+
+/**
+ * @author qiwenbo
+ * @date 2022/8/19 14:51
+ * @Description
+ */
+public interface PersonEATInterface {
+    void eat();
+}
