7041eb99a9e0351cd42c30070a9cd1230caab89f
qwbzjy <whutqwb@163.com>
2022-08-22 18:46:06 +0800

    处理冲突

--- src/main/java/com/example/demo_test/mapper/UserMapper.java
+++ src/main/java/com/example/demo_test/mapper/UserMapper.java
@@ -4,25 +4,16 @@ import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.core.toolkit.Constants;
-import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.demo_test.po.User;
-<<<<<<< HEAD
-import org.springframework.stereotype.Repository;
-=======
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
->>>>>>> c836a0cba2ea8be10c7483fff9255e2fa2847e1e
+import org.springframework.stereotype.Repository;
 
 /**
  * @author qiwenbo
  * @date 2022/8/17 15:14
  * @Description
  */
-<<<<<<< HEAD
 @Repository
-=======
-@Mapper
->>>>>>> c836a0cba2ea8be10c7483fff9255e2fa2847e1e
 public interface UserMapper extends BaseMapper<User> {
 
     IPage<User> selectUserPage(IPage<User> page, @Param(Constants.WRAPPER) Wrapper<User> wrapper);
