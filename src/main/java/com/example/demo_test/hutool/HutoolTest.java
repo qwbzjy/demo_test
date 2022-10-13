c35032b7b74531730b7fb01a5f91f90f8366eb34
qiwenbo <whutqwb@163.com>
2022-10-13 10:58:58 +0800

    hutool测试

--- src/main/java/com/example/demo_test/hutool/HutoolTest.java
+++ src/main/java/com/example/demo_test/hutool/HutoolTest.java
@@ -7,6 +7,9 @@ import liquibase.pro.packaged.D;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.regex.MatchResult;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * @author qiwenbo
@@ -22,10 +25,25 @@ public class HutoolTest {
 //        System.out.println(IdcardUtil.getMonthByIdCard("422202199112282415"));
 //        DesensitizeUtil.mobilePhone("18049531999");
 //        String pattern = "MM月dd日";
-        FastDateFormat pattern = FastDateFormat.getInstance("MM月dd日");
+//        FastDateFormat pattern = FastDateFormat.getInstance("MM月dd日");
 
-        System.out.println(pattern.format(new Date()));
+//        System.out.println(pattern.format(new Date()));
 //        String da = DateUtil.formatChineseDate(new Date(), false, false);
 //        System.out.println(da.split("年")[1]);
+
+        String str = "https://www.runoob.com:80/html/html-tutorial.html";
+        String pattern = " /(\\w+):\\/\\/([^/:]+)(:\\d*)?([^# ]*)/";
+        Pattern compile = Pattern.compile(pattern);
+        Matcher matcher = compile.matcher(str);
+//        String[] split = str.split(pattern);
+//        for (String item : split) {
+//            System.out.println(item);
+//        }
+//        System.out.println(matcher.matches());
+        while(matcher.find()){
+            String group = matcher.group(0);
+            System.out.println(group);
+        }
+
     }
 }
